74d2886bfe70f38b7e19c3a534dfe50d6d0e30a8
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-05-29 13:48:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-//MySQL clásico
+//MySQL PDO
 require_once 'mysql-login.php';
-//Conectando
-$con = mysql_connect($hostname, $username, $password);
-//Manejo de errores
-if (!$con)
-die("Falló la conexión a MySQL: " . mysql_error());
-else
-echo "Conexión exitosa!";
-//Seleccionar base de datos
-mysql_select_db($database)
-or die("Seleccion de base de datos fallida " . mysql_error());
-mysql_close();
+try {
+$con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+print "Conexión exitosa!";
+}
+catch (PDOException $e) {
+print "¡Error!: " . $e->getMessage() . "
+";
+die();
+}
+$con =null;
 ?>
