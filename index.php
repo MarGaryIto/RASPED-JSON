a6504f25a59fb4b733f278d5ce7470904b0270ca
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-05-29 14:09:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   //conexion por MySQL PDO
-  require_once 'mysql-login.php';
+  require_once 'content/mysql-login.php';
   try {
     $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
     print "Conexión exitosa!";
