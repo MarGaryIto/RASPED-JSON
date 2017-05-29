e4e637be38d37b4773d77321bf85baca9b846865
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-05-29 14:11:23 -0500

    Create l.php

---
+++ content/l.php
@@ -0,0 +1,7 @@
+<?php
+  //Datos de conexión a la base de datos
+  $hostname = 'jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
+  $database = 'agxe4dwzsdjevn9l';
+  $username = 'gcco4yk6co4njuol';
+  $password = 'lxhp0mnq7iicqc32';
+?>
