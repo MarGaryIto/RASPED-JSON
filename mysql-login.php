dc403a4fdc4f5c5a2bebc8724868c1178ac2f3cf
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-05-29 13:46:43 -0500

    Update mysql-login.php

--- mysql-login.php
+++ mysql-login.php
@@ -1,6 +1,6 @@
 <?php
   //Datos de conexión a la base de datos
-  $hostname = '	jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+  $hostname = '	jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
   $database = 'agxe4dwzsdjevn9l';
   $username = '	gcco4yk6co4njuol';
   $password = 'lxhp0mnq7iicqc32';
