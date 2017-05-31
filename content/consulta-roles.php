a448bf6bf80c90c429ebbcee14c06dc896bc82b0
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-05-31 12:52:25 -0500

    Update consulta-roles.php

--- content/consulta-roles.php
+++ content/consulta-roles.php
@@ -3,23 +3,22 @@
       require_once 'mysql-login.php';
       try {
         $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
-        print "Conexión exitosa!";
+        $query = "SELECT * FROM Roles";
+        print("<table>");
+        $resultado = $con->query($query); 
+        foreach ( $resultado as $rows) { 
+          print("<tr>");
+          print("<td>".$rows["id_rol"]."</td>");
+          print("<td>".$rows["nombre_rol"]."</td>");
+          print("</tr>"); 
+        }
+        print("</table>");
+        $resultado =null;
       }
         catch (PDOException $e) {
         print "¡Error!: " . $e->getMessage() . "
         ";
         die();
       }
-      //$con =null;
-  $query = "SELECT * FROM Roles";
-  print("<table>");
-  $resultado = $con->query($query); 
-  foreach ( $resultado as $rows) { 
-    print("<tr>");
-    print("<td>".$rows["id_rol"]."</td>");
-    print("<td>".$rows["nombre_rol"]."</td>");
-    print("</tr>"); 
-  }
-  print("</table>");
-  //$resultado =null;
+      $con =null;
 ?>
