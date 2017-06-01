4453f70ead89f266d04740fb2b809a51a4d75698
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-01 17:38:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,16 @@
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>RASPED</title>
-    <link rel="icon" type="image/ico" href="images/logo.ico" />
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container">
-    <?php
-      require_once 'content/mysql-login.php';
-      try {
-        $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
-     ?>
-        <button onclick="window.location.href='content/obtener_roles.php'" class="btn btn-default btn-block">
-          <h3>todos los puestos</h3>
-        </button>
-      <?php
-      }
-        catch (PDOException $e) {
-        print "¡Error!: " . $e->getMessage() . "
-        ";
-        die();
-      }
-      $con =null;*/
-    ?>
-    </div>
-  </body>
-</html>
+<?php
+  //conexion MySQL por PDO
+  
+  //peticion de datos de logg
+  require_once 'mysql-login.php';
+  
+  try {
+    //intento de conexion on los datos de logg
+    $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+    print "Conexión exitosa!";
+  }catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "";
+    die();
+  }
+  $con =null;
+?>
