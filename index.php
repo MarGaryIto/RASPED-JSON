935edb59ff0734f31811aa46820b9c3b857ae42b
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-01 17:54:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,22 @@
       //conexion por MySQL PDO
       require_once 'content/mysql-login.php';
       try {
-        $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+        $DBcon = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+        $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         print "Conexión exitosa!";
+        $query = "SELECT * FROM areas";
+ 
+        $stmt = $DBcon->prepare($query);
+        $stmt->execute();
+        
+        $userData = array();
+
+        while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+
+              $userData['id_area'][] = $row;
+
+        }
+        echo json_encode($userData);
       }
         catch (PDOException $e) {
         print "¡Error!: " . $e->getMessage() . "
