117e2b49d7b4243481694fcdd9b82bc486d44588
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-01 17:42:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
-<?php
-  //conexion MySQL por PDO
-  
-  //peticion de datos de logg
-  require_once 'mysql-login.php';
-  
-  try {
-    //intento de conexion on los datos de logg
-    $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
-    print "Conexión exitosa!";
-  }catch (PDOException $e) {
-    print "¡Error!: " . $e->getMessage() . "";
-    die();
-  }
-  $con =null;
-?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>RASPED</title>
+    <link rel="icon" type="image/ico" href="images/logo.ico" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php
+      //conexion por MySQL PDO
+      require_once 'content/mysql-login.php';
+      try {
+        $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+        print "Conexión exitosa!";
+      }
+        catch (PDOException $e) {
+        print "¡Error!: " . $e->getMessage() . "
+        ";
+        die();
+      }
+      $con =null;
+    ?>
+    <div class="container">
+      <button onClick="window.location.href='#'>boton</button>
+    </div>
+  </body>
+</html>
