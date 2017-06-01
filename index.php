fde675e6f3818cc1402a74f2264713694b308d97
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-01 11:40:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
         print "Conexión exitosa!";
      ?>
-        <button onclick="window.location.href='content/consulta-roles.php'" class="btn btn-default btn-block">
+        <button onclick="window.location.href='content/obtener_roles.php'" class="btn btn-default btn-block">
           <h3>todos los puestos</h3>
         </button>
       <?php
