9398f37129f302fde4fce83bc96c5263af4d1f22
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-02 09:33:07 -0500

    Update roles.php

--- content/roles.php
+++ content/roles.php
@@ -1,2 +1,27 @@
 <?php
+require_once 'mysql-login.php';
+try{
+  
+  $DBcon = new PDO("mysql:host=$hostname ;dbname=$database ",$username ,$password );
+  $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+ }catch(PDOException $ex){
+  
+  die($ex->getMessage());
+ }
+
+$query = "SELECT * FROM roles";
+ 
+$stmt = $DBcon->prepare($query);
+$stmt->execute();
+
+$userData = array();
+
+while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+  
+      $userData['AllRoles'][] = $row;
+ 
+}
+
+echo json_encode($userData);
 ?>
