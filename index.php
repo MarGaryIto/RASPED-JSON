3fdbf2b17081245d5753c48be51fe9d090f61630
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-16 08:56:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,24 @@
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>RASPED</title>
-    <link rel="icon" type="image/ico" href="images/logo.ico" />
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container">
-    <?php
-      //conexion por MySQL PDO
-      require_once 'content/mysql-login.php';
-      try {
-        $con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
-        print "Conexión exitosa!";
-     ?>
-      <br/>
-        <button onclick="window.location.href='content/roles.php'" class="btn btn-default btn-block">
-          <h3>todos las areas</h3>
-        </button>
-      <?php
-      }
-        catch (PDOException $e) {
-        print "¡Error!: " . $e->getMessage() . "
-        ";
-        die();
-      }
-      $con =null;
-    ?>
-    </div>
-  </body>
-</html>
+<?php
+require_once "conectar.php";
+
+$sql = "select * from sedes";
+$res = mysql_query($sql, Conectar::con());
+$row = mysql_fetch_Assoc($res);
+$json = array(
+    'id_sede' => $row['id_sede'],
+    'nombre_sede' => $row['nombre_sede'],
+    'Habilidades' => array()
+);
+
+
+foreach($row as $val){
+    $json['Habilidades'][] = $val;
+}
+
+$jsonencoded = json_encode($json,JSON_UNESCAPED_UNICODE);
+
+$fh = fopen($row['username'].".json", 'w');
+fwrite($fh, $jsonencoded);
+fclose($fh);
+
+?>
