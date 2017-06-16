55dc0fb153c3a1778d433a016f850fd9607a631c
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-16 09:05:16 -0500

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -2,10 +2,10 @@
 //conectar.php
 Class Conectar
 {
-    define(HOST, 'localhost');
-    define(USER, 'root');
-    define(PASS, '');
-    define(DB, 'prueba');
+    define(HOST, 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+    define(USER, 'rftpzxoddr4wj8r6');
+    define(PASS, 'dy44wpzriur1y3x2');
+    define(DB, 'uw66w2of7cy982x1');
 
     public static function con(){
         $con = mysql_connect(HOST,USER,PASS);
