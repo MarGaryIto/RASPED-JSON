cbaa28905d735f9afb030e64fc7203543a60e073
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-22 17:46:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>rasped-json</title>
 </head>
 <body>
-	<a href="">contraseña-telefono-id_usuario</a>
+	<a href="content/mysql-login.php">contraseña-telefono-id_usuario</a>
 
 </body>
 </html>
