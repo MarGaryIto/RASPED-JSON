19f7a17a0a434e0c9b1bc08018e67ba95383358d
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-22 17:43:33 -0500

    Create sedes.php

---
+++ content/sedes.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once ('mysql-login.php');
+
+//Creamos la conexión
+$conexion = mysqli_connect($server, $user, $pass,$bd) 
+or die("Ha sucedido un error inexperado en la conexion de la base de datos");
+
+//generamos la consulta
+$sql = "SELECT * FROM sedes";
+mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
+
+if(!$result = mysqli_query($conexion, $sql)) die();
+
+$clientes = array(); //creamos un array
+
+while($row = mysqli_fetch_array($result)) 
+{ 
+	$id_sede=$row['id_sede'];
+	$nombre_sede=$row['nombre_sede'];	
+
+	$clientes[] = array('id_sede'=> $id_sede, 'nombre_sede'=> $nombre_sede);
+
+}
+	
+//desconectamos la base de datos
+$close = mysqli_close($conexion) 
+or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
+  
+
+//Creamos el JSON
+//$clientes['clientes'] = $clientes;
+$json_string = json_encode($clientes);
+echo $json_string;
+
+//Si queremos crear un archivo json, sería de esta forma:
+/*
+$file = 'clientes.json';
+file_put_contents($file, $json_string);
+*/
+	
+
+?>
