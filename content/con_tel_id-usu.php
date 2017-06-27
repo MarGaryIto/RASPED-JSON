81b0c533189dffb9856fa7174853b02d896b73c2
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-27 11:55:29 -0500

    Update con_tel_id-usu.php

--- content/con_tel_id-usu.php
+++ content/con_tel_id-usu.php
@@ -6,12 +6,8 @@ require_once ('mysql-login.php');
 $conexion = mysqli_connect($server, $user, $pass,$bd) 
 or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 
-//almacenamos variables
-$contrasena = $_REQUEST['contrasena'];
-$telefono = $_REQUEST['telefono'];
-
-//Generamos la colsulta
-$sql = "SELECT * from personal;
+//generamos la consulta
+$sql = "SELECT * FROM personal";
 mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
 
 if(!$result = mysqli_query($conexion, $sql)) die();
@@ -20,21 +16,18 @@ $clientes = array(); //creamos un array
 
 while($row = mysqli_fetch_array($result)) 
 { 
-	$contrasena=$row['contrasena'];
-	$telefono=$row['telefono'];
-	$id_personal=$row['id_personal'];
+    $id=$row['id_personal'];
 
-	$clientes[] = array('contrasena'=> $contrasena, 'telefono'=> $telefono,'id_personal'=>$id_personal);
+    $clientes[] = array('id_personal'=> $id_personal);
 
 }
-	
+    
 //desconectamos la base de datos
 $close = mysqli_close($conexion) 
 or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
   
 
 //Creamos el JSON
-//$clientes['clientes'] = $clientes;
 $json_string = json_encode($clientes);
 echo $json_string;
 
@@ -43,6 +36,5 @@ echo $json_string;
 $file = 'clientes.json';
 file_put_contents($file, $json_string);
 */
-	
-
+    
 ?>
