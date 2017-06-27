e7193e2207012eedff6fda53d20130244c032000
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-27 11:56:50 -0500

    Update con_tel_id-usu.php

--- content/con_tel_id-usu.php
+++ content/con_tel_id-usu.php
@@ -7,7 +7,7 @@ $conexion = mysqli_connect($server, $user, $pass,$bd)
 or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 
 //generamos la consulta
-$sql = "SELECT * FROM personal";
+$sql = "SELECT id_personal FROM personal";
 mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
 
 if(!$result = mysqli_query($conexion, $sql)) die();
