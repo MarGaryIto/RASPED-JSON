e25f1bab5a1c1c6897a8e9a334a2f4cc414dfa9c
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-28 12:39:10 -0500

    Update personal.php

--- content/personal.php
+++ content/personal.php
@@ -6,24 +6,35 @@ or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 //solicitamos las variables
 $telefono = $_REQUEST['telefono'];
 //generamos la consulta
-$sql = "SELECT P.contrasena,
-concat(T.fk_lada,T.telefono) as telefono,
-P.id_personal,
-P.fk_usuario
-FROM personal P, telefonos T
-WHERE P.fk_telefono = T.id_telefono and
-concat(T.fk_lada,T.telefono) = '$telefono'";
+$sql = "SELECT PER.id_personal,
+concat(CUP.fk_sede,CUP.cupo) as cupo,
+PER.nombre_personal,
+concat (PER.apellido_p,' ',PER.apellido_m) as apellidos,
+concat(TEL.fk_lada,TEL.telefono) as telefono,
+PER.contrasena,
+HOR.hr_nombre as horario,
+PUE.nombre_puesto as puesto,
+USU.usuario
+FROM personal PER, telefonos TEL, cupos CUP, horarios HOR, puestos PUE,usuarios USU
+WHERE PER.fk_cupo = CUP.id_cupo and
+PER.fk_telefono = TEL.id_telefono and
+PER.fk_horario = HOR.id_horario and
+PER.fk_puesto = PUE.id_puesto and
+PER.fk_usuario = USU.id_usuario";
 mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
 if(!$result = mysqli_query($conexion, $sql)) die();
 $clientes = array(); //creamos un array
 while($row = mysqli_fetch_array($result)) 
 { 
     $id_personal=$row['id_personal'];
-    $telefono=$row['telefono'];
+    $cupo=$row['cupo'];
+    $apellidos=$row['telefono'];
     $contrasena=$row['contrasena'];
-    $fk_usuario=$row['fk_usuario'];
-    
-    $clientes[] = array('id_personal'=> $id_personal, 'contrasena'=> $contrasena,'telefono'=> $telefono,'fk_usuario'=>$fk_usuario);
+    $horario=$row['horario'];
+    $puesto=$row['puesto'];
+    $usuario=$row['usuario'];
+
+    $clientes[] = array('id_personal'=> $id_personal, 'cupo'=> $cupo, 'telefono'=> $telefono, 'contrasena'=> $contrasena, 'horario'=> $horario, 'puesto'=> $puesto, 'usuario'=> $usuario);
 }
     
 //desconectamos la base de datos
