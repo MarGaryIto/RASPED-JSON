9097b959781a286d1825cc2bf9d9c5a0bb9926cf
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-06-29 10:32:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,11 @@
     <title>Plantilla básica de Bootstrap</title>
  
     <!-- CSS de Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
- 
-    <!-- librerías opcionales que activan el soporte de HTML5 para IE8 -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+
   </head>
   <body>
     <h1>¡Hola mundo!</h1>
@@ -21,9 +19,9 @@
     <!-- Librería jQuery requerida por los plugins de JavaScript -->
     <script src="http://code.jquery.com/jquery.js"></script>
  
-    <!-- Todos los plugins JavaScript de Bootstrap (también puedes
-         incluir archivos JavaScript individuales de los únicos
-         plugins que utilices) -->
-    <script src="js/bootstrap.min.js"></script>
+    <!-- Todos los plugins JavaScript de Bootstrap -->
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <script src="bootstrap/js/npm.js"></script>
   </body>
 </html>
