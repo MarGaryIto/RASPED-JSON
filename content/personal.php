9ce47bc6e7894c0fe26138807b9da96d128baab5
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-05 11:56:22 -0500

    Update personal.php

--- content/personal.php
+++ content/personal.php
@@ -8,10 +8,13 @@ or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 
 //generamos la consulta
 $sql = "SELECT PER.id_personal,
-concat(CUP.fk_sede,CUP.cupo) as cupo,
+CUP.fk_sede as sede,
+CUP.cupo as cupo,
 PER.nombre_personal,
-concat (PER.apellido_p,' ',PER.apellido_m) as apellidos,
-concat(TEL.fk_lada,TEL.telefono) as telefono,
+PER.apellido_p,
+PER.apellido_m,
+TEL.fk_lada as lada,
+TEL.telefono,
 PER.contrasena,
 HOR.hr_nombre as horario,
 PUE.nombre_puesto as puesto,
@@ -24,21 +27,24 @@ PER.fk_puesto = PUE.id_puesto and
 PER.fk_usuario = USU.id_usuario";
 
 mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
-
 if(!$result = mysqli_query($conexion, $sql)) die();
 $clientes = array(); //creamos un array
+
 while($row = mysqli_fetch_array($result)) 
 { 
     $id_personal=$row['id_personal'];
+    $sede=$row['sede'];
     $cupo=$row['cupo'];
     $nombre_personal=$row['nombre_personal'];
-    $apellidos=$row['apellidos'];
+    $apellido_p=$row['apellido_p'];
+    $apellido_m=$row['apellido_m'];
+    $lada=$row['lada'];
     $telefono=$row['telefono'];
     $contrasena=$row['contrasena'];
     $horario=$row['horario'];
     $puesto=$row['puesto'];
     $usuario=$row['usuario'];
-    $clientes[] = array('id_personal'=> $id_personal, 'cupo'=> $cupo, 'nombre_personal'=> $nombre_personal, 'apellidos'=>$apellidos, 'telefono'=> $telefono, 'contrasena'=> $contrasena, 'horario'=> $horario, 'puesto'=> $puesto, 'usuario'=> $usuario);
+    $clientes[] = array('id_personal'=> $id_personal, 'sede'=>$sede , 'cupo'=> $cupo, 'nombre_personal'=> $nombre_personal, 'apellido_p'=> $apellido_p, 'apellido_m'=> $apellido_m, 'lada'=> $lada, 'telefono'=> $telefono, 'contrasena'=> $contrasena, 'horario'=> $horario, 'puesto'=> $puesto, 'usuario'=> $usuario );
 }
     
 //desconectamos la base de datos
