9b16e07087d77c058d2f6821d21005a51f3638b0
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-05 16:03:49 -0500

    Update insertar_personal.php

--- content/insertar_personal.php
+++ content/insertar_personal.php
@@ -8,7 +8,9 @@ or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 //generamos la consulta
 $sql = "INSERT INTO personal(nombre_personal) values('panfilo')";
 
-mysql_query($sql);
+$insertar = mysql_query($conexion,$sql);
+
+if (!$insertar){echo "Error al guardar";}else{echo "Guardado con exito";}
 
 //desconectamos la base de datos
 $close = mysqli_close($conexion) 
