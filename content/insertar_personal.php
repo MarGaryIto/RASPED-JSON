b0a3416b9d7e0875687732ac3951e2b4c20a26e9
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-05 15:58:05 -0500

    Update insertar_personal.php

--- content/insertar_personal.php
+++ content/insertar_personal.php
@@ -1,1 +1,17 @@
+<?php
+require_once ('mysql-login.php');
 
+//Creamos la conexión
+$conexion = mysqli_connect($server, $user, $pass,$bd) 
+or die("Ha sucedido un error inexperado en la conexion de la base de datos");
+
+//generamos la consulta
+$sql = "INSERT INTO personal(nombre_personal) values('panfilo')";
+
+mysql_query($sql);
+
+//desconectamos la base de datos
+$close = mysqli_close($conexion) 
+or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
+
+?>
