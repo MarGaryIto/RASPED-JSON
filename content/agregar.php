1a59fa8d895a1daa7902f2022ec733ed2dd70cfb
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-13 12:17:06 -0500

    Update agregar.php

--- content/agregar.php
+++ content/agregar.php
@@ -12,18 +12,18 @@
 	require_once ('mysql-login.php');
 
 	//ejecucion de conexion o devolucion de error
-	@$conexion = mysqli_connect($server, $user, $pass,$bd);;
+	$conexion = mysqli_connect($server, $user, $pass,$bd);;
 	if (!$conexion) {
 		die('Connect Error: '.mysqli_connect_error());
 	}
-
+	/*
 	//generacion de consulta para insertar datos
 	$query  = "insert into personal(nombre_personal,apellido_m,apellido_p,contrasena) values('$nombre_personal','$apellido_m','$apellido_p','$contrasena');
 
 	//realizar la inserccion o devolucion de error
 	$result = mysqli_query($conexion, $query) or die('Error:'.mysqli_error());
 
-	mysqli_close($conexion);
+	mysqli_close($conexion);*/
 	
 	echo "inserccion finalizada";
 ?>
