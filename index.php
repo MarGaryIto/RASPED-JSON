d59eada558ef60a1b49f1aa346327b0ef29259d3
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-14 11:20:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <title>RASPED</title>
  
     <!-- CSS de Bootstrap -->
-    <link rel="stylesheet" href="https://goo.gl/ixnP5g"
-      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet">
     <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
