27fe94e8ac01ddfe34b24897d12cd44326737da4
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-14 12:42:47 -0500

    Update agregar.php

--- content/agregar.php
+++ content/agregar.php
@@ -8,14 +8,20 @@
 	$apellido_m = $_POST['apellido_m'];
 	$apellido_p = $_POST['apellido_p'];
 	$contrasena = $POST['contrasena'];
+	
+
 	//llamar mysql-login.php que contiene los datos de la base de datos para conectar
 	require_once ('mysql-login.php');
+
 	//ejecucion de conexion o devolucion de error
 	$conexion = mysqli_connect($server, $user, $pass,$bd);;
 	if (!$conexion) {
 		die('Connect Error: '.mysqli_connect_error());
 	}
-	mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
+
+	//estándar de codificación Unicode Transformation 8 bits para compatibilidad ASCII
+	mysqli_set_charset($conexion, "utf8");
+
 	//consultas - inserccion de cupos y telefonos
 	$query_insert_sede = "insert ignore into sedes(id_sede) values ('$sede')";
 	$query_insert_cupo = "insert into cupos(fk_sede,cupo) values ('$sede','$cupo')";
@@ -26,29 +32,37 @@
 	$result = mysqli_query($conexion, $query_insert_cupo) or die('Error:'.mysqli_error());
 	$result = mysqli_query($conexion, $query_insert_lada) or die('Error:'.mysqli_error());
 	$result = mysqli_query($conexion, $query_insert_telefono) or die('Error:'.mysqli_error());
+
 	//encriptacion md5 de contraseña
 	$contrasena = md5($contrasena);
-	/*//generamos la consulta
-	$sql = "SELECT id_cupo from cupos WHERE fk_sede = '@sede' and cupo = '@cupo'";
+
+	//query para consulta de fk_cupo y fk_telefono
+	$query_select_fk_cupo = "SELECT id_cupo from cupos WHERE fk_sede = '$sede' and cupo = '$cupo'";
+	$query_select_fk_telefono = "SELECT id_telefono from telefonos WHERE fk_lada='$lada' and telefono='$telefono'";
 	
-	if(!$result = mysqli_query($conexion, $sql)) die();
-	$clientes = array(); //creamos un array
-	while($row = mysqli_fetch_array($result)) 
-	{ 
-	    $id_personal=$row['id_personal'];
-	    $cupo=$row['cupo'];
-	    $nombre_personal=$row['nombre_personal'];
-	    $apellidos=$row['apellidos'];
-	    $telefono=$row['telefono'];
-	    $contrasena=$row['contrasena'];
-	    $horario=$row['horario'];
-	    $puesto=$row['puesto'];
-	    $usuario=$row['usuario'];
-	    $clientes[] = array('id_personal'=> $id_personal, 'cupo'=> $cupo, 'nombre_personal'=> $nombre_personal, 'apellidos'=>$apellidos, 'telefono'=> $telefono, 'contrasena'=> $contrasena, 'horario'=> $horario, 'puesto'=> $puesto, 'usuario'=> $usuario);
+	//ejecucion de query para consulta de fk_cupo y fk_telefono o arrojo de error
+	if(!$result_fk_cupo = mysqli_query($conexion, $query_select_fk_cupo)) die('Error:'.mysqli_error());
+	if(!$result_fk_telefono = mysqli_query($conexion, $query_select_fk_telefono)) die('Error:'.mysqli_error());
+
+	//captura de fk_cupo mediante ciclo while
+	while($row = mysqli_fetch_array($result_fk_cupo)) { 
+	    $fk_cupo=$row['id_cupo'];
 	}
-	//realizar la inserccion o devolucion de error*/
+	//captura de fk_telefono mediante ciclo while
+	while($row = mysqli_fetch_array($result_fk_cupo)) { 
+	    $fk_telefono=$row['id_telefono'];
+	}
 	
-	mysqli_close($conexion);
+	//query para la inserccion de usuario
+	$query_insert_personal = "insert into 
+	personal(fk_cupo,nombre_personal,apellido_m,apellido_p,
+	fk_telefono,contrasena)
+	values('$fk_cupo','$nombre_personal','$apellido_m','$apellido_p',
+	'$fk_telefono','$contrasena')";
+
+	$result_insert_personal = mysql_query($conexion,$sql);
+	if (!$result_insert_personal){echo "Error al guardar";}else{echo "Guardado con exito";}
 	
-	echo "inserccion finalizada";
+	mysqli_close($conexion)or die("Error en desconeccion");
+	
 ?>
