ec56faec335c7c5da99fc151e19629c551ffcfe8
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-19 10:46:28 -0500

    Update insertar_asistencia.php

--- content/insertar_asistencia.php
+++ content/insertar_asistencia.php
@@ -42,9 +42,9 @@
   if(!$result_id_asistencia = mysqli_query($conexion, $query_select_asistencia)) die('Error:'.mysqli_error());
 
   //captura de fk_cupo mediante ciclo while
-  $asistencias = false;
+  $asistencias = "false";
   while($row = mysqli_fetch_array($result_id_asistencia)) { 
-      $asistencias = true;
+      $asistencias = "true";
   }
 
   echo $asistencias;
