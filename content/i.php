a5b740d95c0a72a1c4add30eea4fdc92b5317ae5
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-24 10:11:39 -0500

    Update i.php

--- content/i.php
+++ content/i.php
@@ -3,7 +3,7 @@
 <head>
   <title>complete example</title>
 </head>
-<body background="https://previews.123rf.com/images/benedikt21/benedikt211206/benedikt21120600011/13990317-filtrar-el-zumo-de-naranja-en-la-cocina-Foto-de-archivo.jpg" width="100%" height="100%">
+<body background="https://previews.123rf.com/images/benedikt21/benedikt211206/benedikt21120600011/13990317-filtrar-el-zumo-de-naranja-en-la-cocina-Foto-de-archivo.jpg" width="100%">
 
 </body>
 </html>
