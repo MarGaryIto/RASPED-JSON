e600f849818fbcb42cd6efea1d1c67244c22bc50
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-07-27 10:24:57 -0500

    Update horarios.php

--- content/horarios.php
+++ content/horarios.php
@@ -7,11 +7,11 @@ $conexion = mysqli_connect($server, $user, $pass,$bd)
 or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 
 //generamos la consulta
-$sql = "SELECT
-*
-FROM
-horarios
-";
+$sql = "select P.id_personal, A.id_asistencias, P.nombre_personal,
+P.apellido_m, P.apellido_p, F.fecha, A.hr_entrada,
+A.hr_comida_i, A.hr_comida_f, A.hr_salida
+from personal P, asistencias A, fechas F
+where A.fk_personal = P.id_personal and A.fk_fecha = F.id_fecha";
 
 mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
 if(!$result = mysqli_query($conexion, $sql)) die();
@@ -19,15 +19,19 @@ $clientes = array(); //creamos un array
 
 while($row = mysqli_fetch_array($result)) 
 { 
-    $id_horario=$row['id_horario'];
+    $id_personal=$row['id_personal'];
+    $id_asistencias=$row['id_asistencias'];
+    $nombre_personal=$row['nombre_personal'];
+    $apellido_m=$row['apellido_m'];
+    $apellido_p=$row['apellido_p'];
+    $fecha=$row['fecha'];
     $hr_entrada=$row['hr_entrada'];
     $hr_comida_i=$row['hr_comida_i'];
     $hr_comida_f=$row['hr_comida_f'];
     $hr_salida=$row['hr_salida'];
-    $tolerancia=$row['tolerancia'];
-    $hr_nombre=$row['hr_nombre'];
 
-    $clientes[] = array('id_horario'=> $id_horario, 'hr_entrada'=> $hr_entrada, 'hr_comida_i'=> $hr_comida_i, 'hr_comida_f'=> $hr_comida_f, 'hr_salida'=> $hr_salida, 'tolerancia'=> $tolerancia, 'hr_nombre'=> $hr_nombre );
+    $clientes[] = array('id_personal'=> $id_personal, 'id_asistencias'=> $id_asistencias, 'nombre_personal'=> $nombre_personal, 'apellido_m'=> $apellido_m, 'apellido_p'=> $apellido_p,
+                        'fecha'=> $fecha, 'hr_entrada'=> $hr_entrada, 'hr_comida_i'=> $hr_comida_i, 'hr_comida_f'=> $hr_comida_f, 'hr_salida'=> $hr_salida,  );
 }
     
 //desconectamos la base de datos
