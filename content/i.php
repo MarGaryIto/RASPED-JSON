241e6feb0c28de671b659c89fe127b76de792a59
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-08-02 11:51:02 -0500

    Delete i.php

--- content/i.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>complete example</title>
-</head>
-<body background="https://previews.123rf.com/images/benedikt21/benedikt211206/benedikt21120600011/13990317-filtrar-el-zumo-de-naranja-en-la-cocina-Foto-de-archivo.jpg" width="100%">
-
-</body>
-</html>
