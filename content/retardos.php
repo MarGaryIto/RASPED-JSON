f60e143cb9bad7d48f411a58e3eab0057f603e60
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-08-10 17:08:08 -0500

    Update retardos.php

--- content/retardos.php
+++ content/retardos.php
@@ -7,12 +7,13 @@ $conexion = mysqli_connect($server, $user, $pass,$bd)
 or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 
 //generamos la consulta
-$sql = "select P.id_personal, concat(C.fk_sede,C.cupo) as cupo, R.id_retardos, P.nombre_personal, P.apellido_m, P.apellido_p,
-F.fecha, R.hr_entrada, R.hr_comida_i, R.hr_comida_f, R.hr_salida
-from personal P, retardos R, fechas F, cupos C
-where R.fk_personal = P.id_personal and
-R.fk_fecha = F.id_fecha and
-P.fk_cupo = C.id_cupo;";
+$sql = "select P.id_personal, concat(C.fk_sede,C.cupo) as cupo, A.id_asistencias, P.nombre_personal, P.apellido_m, P.apellido_p,
+F.fecha, A.hr_entrada, A.hr_comida_i, A.hr_comida_f, A.hr_salida
+from personal P, asistencias A, fechas F, cupos C
+where A.fk_personal = P.id_personal and
+A.fk_fecha = F.id_fecha and
+P.fk_cupo = C.id_cupo and
+retardo = true;";
 mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
 
 //ejecución de la consulta
@@ -25,7 +26,7 @@ $clientes = array();
 while($row = mysqli_fetch_array($result)) { 
     $id_personal=$row['id_personal'];
     $cupo=$row['cupo'];
-    $id_retardos=$row['id_retardos'];
+    $id_asistencias=$row['id_asistencias'];
     $nombre_personal=$row['nombre_personal'];
     $apellido_m=$row['apellido_m'];
     $apellido_p=$row['apellido_p'];
@@ -34,7 +35,7 @@ while($row = mysqli_fetch_array($result)) {
     $hr_comida_i=$row['hr_comida_i'];
     $hr_comida_f=$row['hr_comida_f'];
     $hr_salida=$row['hr_salida'];
-    $clientes[] = array('id_personal'=> $id_personal,'cupo'=>$cupo, 'id_retardos'=> $id_retardos, 'nombre_personal'=> $nombre_personal, 'apellido_m'=> $apellido_m, 'apellido_p'=> $apellido_p,
+    $clientes[] = array('id_personal'=> $id_personal, 'cupo'=> $cupo, 'id_asistencias'=> $id_asistencias, 'nombre_personal'=> $nombre_personal, 'apellido_m'=> $apellido_m, 'apellido_p'=> $apellido_p,
                         'fecha'=> $fecha, 'hr_entrada'=> $hr_entrada, 'hr_comida_i'=> $hr_comida_i, 'hr_comida_f'=> $hr_comida_f, 'hr_salida'=> $hr_salida,  );
 }
     
